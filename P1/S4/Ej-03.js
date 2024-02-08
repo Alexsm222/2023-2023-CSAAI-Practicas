4c96577b0576535e3383510855849471dd7eba2c
alexsas <ca.sas.2020@alumnos.urjc.es>
2024-02-08 20:31:52 +0100

    Un poco de javascript y alguna actualización

---
+++ P1/S4/Ej-03.js
@@ -0,0 +1,3 @@
+console.log("Estoy fuera del HTML");
+console.log("Se recomienda hacerlo así");
+console.log("------");
---
+++ P1/S4/Ej-06.js
@@ -0,0 +1,9 @@
+function manejador_parrafo()
+{
+    console.log("Clic sobre parrafo...")
+}
+console.log("Ejecutando js...")
+
+const test = document.getElementById("test")
+
+test.onclick = manejador_parrafo;
