87efb4c82930497d0d0745eef832c555b0ca323b
alexsas <ca.sas.2020@alumnos.urjc.es>
2024-03-14 10:40:17 +0100

    Animaciones y un poco de calculadora

---
+++ P2/S5-JavaSqII/Ej_10.js
@@ -0,0 +1,13 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el párrafo
+const test = document.getElementById('test');
+const button = document.getElementById('button');
+
+//-- Función de retrollamada de la pulsación del párrafo
+button.onclick = () => {
+    console.log("Click!");
+
+    
+
+}
